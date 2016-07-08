be05bc9bbdd4d6c409d8d63b219b2c134f79ea5f
Laurence kite <laurencekite@Laurences-MacBook-Pro-2.local>
2016-07-08 11:17:29 +0100

    updated codde with full and empty methods

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -8,13 +8,20 @@ class DockingStation
   end
 
   def release_bike
-    fail 'No bikes available' if @bikes.empty?
+    fail 'No bikes available' if empty?
     @bikes.pop
   end
 
   def dock(bike)
-    fail 'Docking station full' if @bikes.length > 19
+    fail 'Docking station full' if full?
     @bikes << bike
   end
-
+  private
+  def full?
+  @bikes.length >= 20 ? true : false
+  end
+  
+  def empty?
+    @bikes.empty?
+  end
 end
